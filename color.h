4947566000c8e89a8f21233c8a5870232e6b596d
William Cross <WilliamCross.CS@gmail.com>
2024-07-23 01:05:09 -0400

    add .gitignore

--- color.h
+++ color.h
@@ -17,6 +17,7 @@ void write_color(std::ostream& out, const color& pixel_color){
     int gbyte = int(255.999 * g);
     int bbyte = int(255.999 * b);
 
+    // Write out the pixel color components
     out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
 }
 
